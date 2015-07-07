47c027cc0a460b9c3cdaa7f427e30003bcdcc26e
rahulraopune <rahulraopune@gmail.com>
2015-07-07 12:18:56 +0530

    lex symtab

---
+++ sample.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a=0,b=0,c=0;
+	printf("\nEnter the value for a	:");
+	scanf("%d",a);
+	printf("\nEnter the value for d	:");
+	scanf("%d",b);
+	c=a+b;
+	printf("\n%d",c);
+	return 0;
+
+}
